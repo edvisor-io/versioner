21c05119c8ec374dcd1d6028f71e1420948025de
John Kor <john.kor@gmail.com>
2019-04-04 12:27:57 -0700

    changed git-checkeven to never "exit 1" and return parsable output

--- bin/git-checkeven.sh
+++ bin/git-checkeven.sh
@@ -24,12 +24,15 @@ nA2B=$(git rev-list --count $revA..$revB)
 nB2A=$(git rev-list --count $revB..$revA)
 
 if [ $nA2B -eq 0 -a $nB2A -eq 0 ]; then
-  printf "$revA is even with $revB\n"
+  # printf "$revA is even with $revB\n"
+  printf "even"
   exit 0
 elif [ $nA2B -gt 0 ]; then
-  printf "$revA is $nA2B commits behind $revB\n"
+  # printf "$revA is $nA2B commits behind $revB\n"
+  printf "behind"
   exit 0
 else
-  printf "$revA is $nB2A commits ahead of $revB\n"
-  exit 1
+  # printf "$revA is $nB2A commits ahead of $revB\n"
+  printf "ahead"
+  exit 0
 fi
